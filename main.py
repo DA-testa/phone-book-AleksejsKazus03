9584d0c5007e9ec08d97d8c0c237963b9086cea1
Aleksejs Kazus 221RDB105 <125798333+AleksejsKazus03@users.noreply.github.com>
2023-04-02 18:16:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
 # python3
 # Aleksejs_Kazus_221RDB105
-n = int(input().strip())
-phone_book = {}
 
-for i in range(n):
-    query = input().strip().split()
-    if query[0] == 'add':
-        phone_book[query[1]] = query[2]
-    elif query[0] == 'del':
-        if query[1] in phone_book:
-            del phone_book[query[1]]
-    elif query[0] == 'find':
-        if query[1] in phone_book:
-            print(phone_book[query[1]])
-        else:
-            print('not found')
 
